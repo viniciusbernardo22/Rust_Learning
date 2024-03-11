c4d2e34090e0ea4c18aad38219b5c0f1cfc2e119
Vinicius <vini383@gmail.com>
2024-03-11 14:39:00 -0300

    add maior_numero fn

--- a7/src/main.rs
+++ a7/src/main.rs
@@ -1,3 +1,19 @@
+
+fn maior_numero(nums: &[i32]) -> i32 {
+
+    let mut maior: i32 = nums[0];
+
+    for i in nums {
+        if i > &maior {
+            maior = *i;
+        }
+    }
+
+    return maior;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums = [3, 7, 20, 9, 10];
+    maior_numero(&nums);
+    println!("{}", maior_numero(&nums));
 }
